c892728dc23249cf92387e23f7d72b9baefe85d8
javierplanta <javierkns@gmail.com>
2017-03-29 16:57:01 -0300

    Creo haber hecho las funciones sigint y la estructura lista

--- src/functions.c
+++ src/functions.c
@@ -1,3 +1,8 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "functions.h"
 
 int isInt(char *number){
@@ -10,4 +15,42 @@ int isInt(char *number){
     }
   }
   return 0;
+}
+
+void nuevoSigint(int num_senal)
+{
+
+	//Obtengo el pid del proceso actual y hago el print del mensaje
+   	int pid_actual = (int)getpid(2);
+   	printf("Soy el hijo con pid: %i, y estoy vivo aun", pid_actual);
+
+   	//Ahora tengo que cambiar el Sigint por el default
+   	signal(SIGINT, defaultSigint);
+
+}
+
+void defaultSigint(int num_senal)
+{
+	//aqui va el default
+	kill(getpid(), sig);
+}
+
+lista *crear_lista(int pid, int hijo) {
+	lista *L = (lista*)malloc(sizeof(lista));
+	L->sgte = NULL;
+	L->pid = pid;
+	L->hijo = hijo;
+	L->contador = 0;
+	return L;
+}
+
+void lista_add(lista *L, int hijo, int pid) {
+	lista *aux = L;
+	while (aux->sgte != NULL) {
+		aux = aux->sgte;
+	}
+	aux->sgte = (lista*)malloc(sizeof(lista));
+	aux->sgte->pid = pid;
+	aux->sgte->hijo = hijo;
+	aux->sgte->sgte = NULL;
 }
--- src/functions.h
+++ src/functions.h
@@ -8,12 +8,48 @@
 #include <signal.h>
 #include <string.h>
 
+//////// --ESTRUCTURAS-- ///////
+
+struct list {
+	int hijo; //guarda el número de hijo
+	int pid; //guarda el pid de ese hijo
+	int contador; //guarda el contador de cuantas veces ha sido llamado
+	struct list *sgte;
+};
+typedef struct list lista;
+
+
+//////// --FUNCIONES-- ////////
+
 /*
   Funcion que verifica si un string representa un numero o no, entrega 0 si es
   un numero y 1 si no.
   @param number string para verificar.
 */
 int isInt(char *number);
+
+/*
+	Funcion a reemplazar el SIGINT y envia el 
+	mensaje pedido por el enunciado.
+*/
+void nuevoSigint(int);
+
+/*
+	Funcion default del SIGINT, que vuelve una vez
+	ejecutado el SIGINT modificado.
+*/
+void defaultSigint(int);
+
+/*
+	Crea una lista con un valor inicial.
+*/
+lista *crear_lista(int pid, int hijo);
+
+/*
+	Agrega un nodo al final de la lista
+	con ciertos valores.
+*/
+void lista_add(lista *L, int hijo, int pid);
 
 
 #endif
